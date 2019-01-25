e58c77f70882a84d5eeb8077744cfa82c880f5ea
Renan Toesqui Magalhães <renan.magalhaes@creativedrive.com>
2019-01-25 17:29:29 -0200

    tmux

--- tmux/tmux.sh
+++ tmux/tmux.sh
@@ -1,3 +1,6 @@
+#verify tmux
+type tmux >/dev/null 2>&1 || { echo >&2 "Install tmux before run this script "; exit 1; }
+
 #install tmp (tmux plugin manager)
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
