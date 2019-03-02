10d27e7c5b6799a956676f1f27aee84366a74d3d
renantmagalhaes <renantmagalhaes@gmail.com>
2019-03-02 14:50:18 -0300

    zsh

--- zsh/zsh.sh
+++ zsh/zsh.sh
@@ -7,11 +7,15 @@ type curl >/dev/null 2>&1 || { echo >&2 "Install zsh before run this script "; e
 #install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-#install zsh-syntax-highlighting.zsh
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
 #syslink 
 ln -s -f $PWD/zshrc ~/.zshrc
+
+#install plugins
+##zsh-syntax-highlighting.zsh
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+##zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 
 #install powerlevel9k
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
