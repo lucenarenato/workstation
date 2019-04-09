135afad65551bf95629cbfeb2e6191d980980ae1
Renan Toesqui Magalhaes <renantmagalhaes@gmail.com>
2019-04-09 12:29:43 +0200

    zsh-completions

--- zsh/zsh.sh
+++ zsh/zsh.sh
@@ -15,6 +15,8 @@ ln -s -f $PWD/zshrc ~/.zshrc
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 ##zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+##zsh-completions
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
 
 
 #install powerlevel9k
